cb1701ba04843317bab730f79c399f23d22238e8
Laksh Singla <lakshsingla@gmail.com>
2018-11-28 22:44:33 +0530

    Extended rangecheck to check for the range of singleton alphabets also

--- rangecheck.pl
+++ rangecheck.pl
@@ -1,9 +1,11 @@
-liesInCSExpr(CSExpr, Val) :- split_string(CSExpr, ",", "", L), liesInExpr(L, Val). 
+lies_in_CS_Expr(CSExpr, Val, IsNum) :- split_string(CSExpr, ",", "", L), lies_in_expr(L, Val, IsNum). 
 
-liesInExpr([H|T], Val) :- satisfiesMaybeRange(H, Val); liesInExpr(T, Val).
+lies_in_expr([H|T], Val, IsNum) :- satisfies_maybe_range(H, Val, IsNum); lies_in_expr(T, Val, IsNum).
 
-satisfiesMaybeRange(Expr, Val) :- split_string(Expr, "-", "", MaybeRange), satisfies(MaybeRange, Val).
+satisfies_maybe_range(Expr, Val, IsNum) :- split_string(Expr, "-", "", MaybeRange), satisfies(MaybeRange, Val, IsNum).
 
-satisfies(MaybeRange, Val) :- 
-	MaybeRange = [R1, R2 | []], number_string(NR1, R1), number_string(NR2, R2), Val >= NR1, Val =< NR2; 
-	MaybeRange = [TestVal | []], number_string(NTestVal, TestVal), Val=NTestVal.
+satisfies(MaybeRange, Val, IsNum) :- 
+	IsNum, MaybeRange = [Num1, Num2 | []], number_string(NNum1, Num1), number_string(NNum2, Num2), number_string(NVal, Val), NVal >= NNum1, NVal =< NNum2; 
+	IsNum, MaybeRange = [Num | []], number_string(NNum, Num), number_string(NVal, Val),  NVal=NNum;
+	\+IsNum, MaybeRange = [Alpha1, Alpha2 | []], Val >= Alpha1, Val =< Alpha2; 
+	\+IsNum, MaybeRange = [Alpha | []], Val=Alpha.
