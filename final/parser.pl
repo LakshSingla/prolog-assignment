3566e69158c2321fd3f1c319d868002aa5214612
Shubham Saxena <shbhmsxn4@gmail.com>
2018-11-28 22:52:52 +0530

    Naming Conventions Changed.

--- final/parser.pl
+++ final/parser.pl
@@ -2,7 +2,12 @@
 
 add_fwrule(Fate, Rule) :- assertz(fwrule(Fate,Rule)).
 
-fate(Fate, PacketStr) :- fwrule(Fate, RuleStr), split_string(RuleStr, " ", "", Rule), split_string(PacketStr, " ", "", Packet), fwrule_matches(Rule, Packet), !.
+fate(Fate, PacketStr) :- 
+	fwrule(Fate, RuleStr),
+	split_string(RuleStr, " ", "", Rule),
+	split_string(PacketStr, " ", "", Packet),
+	fwrule_matches(Rule, Packet),
+	!.
 
 fwrule_matches(Rule, Packet) :- rule_parser(Rule, [], Packet).
 
@@ -15,38 +20,83 @@ clause_parser(X, Y, Packet) :-
 		ip_clause_matches(X, Y, Packet).
 
 adpt_clause_matches(["adapter", AdptExpr|W], W, Packet) :- 
-	get_keyval(Packet, "adapter", PacketAdptExpr), 
-	adpt_expr_matches(AdptExpr,PacketAdptExpr).
+	get_keyval(Packet, "adapter", PktAdpt), 
+	adpt_expr_matches(Adpt,PktAdpt).
 
-eth_clause_matches(["ether", "vid", VidExpr, "proto", ProtoExpr|W], W, Packet) :- 
-	get_keyval(Packet, "vid", PacketVidExpr),
-	num_expr_matches(VidExpr, PacketVidExpr),
-	get_keyval(Packet, "nlproto", PacketProtoExpr),
-	proto_expr_matches(ProtoExpr, PacketProtoExpr).
-eth_clause_matches(["ether", "vid", VidExpr|W], W, Packet) :-
-get_keyval(Packet, "vid", PacketVidExpr),
-	num_expr_matches(VidExpr, PacketVidExpr).
-eth_clause_matches(["ether", "proto", ProtoExpr|W], W, Packet) :-
-	get_keyval(Packet, "nlproto", PacketProtoExpr),
-	proto_expr_matches(ProtoExpr, PacketProtoExpr).
+eth_clause_matches(["ether", "vid", Vid, "proto", NlProto|W], W, Packet) :- 
+	get_keyval(Packet, "vid", PktVid),
+	num_expr_matches(Vid, PktVid),
+	get_keyval(Packet, "nlproto", PktNlProto),
+	proto_expr_matches(NlProto, PktNlProto).
+eth_clause_matches(["ether", "vid", Vid|W], W, Packet) :-
+get_keyval(Packet, "vid", PktVid),
+	num_expr_matches(Vid, PktVid).
+eth_clause_matches(["ether", "proto", NlProto|W], W, Packet) :-
+	get_keyval(Packet, "nlproto", PktNlProto),
+	proto_expr_matches(NlProto, PktNlProto).
 
-ip_clause_matches(["ip", "src", "addr", SrcAddrExpr, "dst", "addr", DstAddrExpr|W0], W, Packet) :- condition_matches(W0, W, Packet).
-ip_clause_matches(["ip", "addr", AddrExpr|W0], W, Packet) :- condition_matches(W0, W, Packet).
-ip_clause_matches(["ip", "src", "addr", SrcAddrExpr|W0], W, Packet) :- condition_matches(W0, W, Packet).
-ip_clause_matches(["ip", "dst", "addr", DstAddrExpr|W0], W, Packet) :- condition_matches(W0, W, Packet).
+ip_clause_matches(["ip", "src", "addr", SrcIp, "dst", "addr", DstIp|W0], W, Packet) :- 
+	get_keyval(Packet, "srcip", PktSrcIp),
+	ip_expr_matches(SrcIp, PktSrcIp),
+	get_keyval(Packet, "dstip", PktDstIp),
+	ip_expr_matches(DstIp, PktDstIp),
+	condition_matches(W0, W, Packet).
+
+ip_clause_matches(["ip", "addr", Ip|W0], W, Packet) :- 
+	get_keyval(Packet, "srcip", PktSrcIp),
+	ip_expr_matches(Ip, PktSrcIp),
+	condition_matches(W0, W, Packet);
+	get_keyval(Packet, "dstip", PktDstIp),
+	ip_expr_matches(Ip, PktDstIp),
+	condition_matches(W0, W, Packet).
+
+ip_clause_matches(["ip", "src", "addr", SrcIp|W0], W, Packet) :- 
+	get_keyval(Packet, "srcip", PktSrcIp),
+	ip_expr_matches(SrcIp, PktSrcIp),
+	condition_matches(W0, W, Packet).
+
+ip_clause_matches(["ip", "dst", "addr", DstIp|W0], W, Packet) :- 
+	get_keyval(Packet, "dstip", PktDstIp),
+	ip_expr_matches(Ip, PktDstIp),
+	condition_matches(W0, W, Packet).
+
 ip_clause_matches(["ip"|W0], W, Packet) :- condition_matches(W0, W, Packet).
 
-condition_matches(["tcp", "dst", "port", DstPortExpr, "src", "port", SrcPortExpr|W], W, Packet) :- true.
-condition_matches(["tcp", "dst", "port", DstPortExpr|W], W, Packet) :- true.
-condition_matches(["tcp", "src", "port", SrcPortExpr|W], W, Packet) :- true.
+condition_matches(["tcp", "dst", "port", DstPort, "src", "port", SrcPort|W], W, Packet) :-
+	get_keyval(Packet, "srcport", PktSrcPort),
+	num_expr_matches(SrcPort, PktSrcPort),
+	get_keyval(Packet, "dstport", PktDstPort),
+	num_expr_matches(DstPort, PktDstPort).
+condition_matches(["tcp", "dst", "port", DstPort|W], W, Packet) :-
+	get_keyval(Packet, "dstport", PktDstPort),
+	num_expr_matches(DstPort, PktDstPort).
+condition_matches(["tcp", "src", "port", SrcPort|W], W, Packet) :-
+	get_keyval(Packet, "srcport", PktSrcPort),
+	num_expr_matches(SrcPort, PktSrcPort).
 
-condition_matches(["udp", "dst", "port", DstPortExpr, "src", "port", SrcPortExpr|W], W, Packet) :- true.
-condition_matches(["udp", "dst", "port", DstPortExpr|W], W, Packet) :- true.
-condition_matches(["udp", "src", "port", SrcPortExpr|W], W, Packet) :- true.
+condition_matches(["udp", "dst", "port", DstPort, "src", "port", SrcPort|W], W, Packet) :-
+	get_keyval(Packet, "srcport", PktSrcPort),
+	num_expr_matches(SrcPort, PktSrcPort),
+	get_keyval(Packet, "dstport", PktDstPort),
+	num_expr_matches(DstPort, PktDstPort).
+condition_matches(["udp", "dst", "port", DstPort|W], W, Packet) :-
+	get_keyval(Packet, "dstport", PktDstPort),
+	num_expr_matches(DstPort, PktDstPort).
+condition_matches(["udp", "src", "port", SrcPort|W], W, Packet) :-
+	get_keyval(Packet, "srcport", PktSrcPort),
+	num_expr_matches(SrcPort, PktSrcPort).
 
-condition_matches(["icmp", "type", TypeExpr, "code", CodeExpr|W], W, Packet) :- true.
-condition_matches(["icmp", "type", TypeExpr|W], W, Packet) :- true.
-condition_matches(["icmp", "code", CodeExpr|W], W, Packet) :- true.
+condition_matches(["icmp", "type", IcmpType, "code", IcmpCode|W], W, Packet) :-
+	get_keyval(Packet, "icmptype", PktIcmpType),
+	num_expr_matches(IcmpType, PktIcmpType),
+	get_keyval(Packet, "icmpcode", PktIcmpCode),
+	num_expr_matches(IcmpCode, PktIcmpCode).
+condition_matches(["icmp", "type", IcmpType|W], W, Packet) :-
+	get_keyval(Packet, "icmptype", PktIcmpType),
+	num_expr_matches(IcmpType, PktIcmpType).
+condition_matches(["icmp", "code", IcmpCode|W], W, Packet) :-
+	get_keyval(Packet, "icmpcode", PktIcmpCode),
+	num_expr_matches(IcmpCode, PktIcmpCode).
 
 get_keyval([Key, Val| _], Key, Val).
 get_keyval([_, _| T], Key, Val) :- get_keyval(T, Key, Val).
@@ -54,6 +104,6 @@ get_keyval([_, _| T], Key, Val) :- get_keyval(T, Key, Val).
 adpt_expr_matches(X, X).
 num_expr_matches(X, X).
 proto_expr_matches(RuleProto, PacketProto) :-
-	alpha_proto(RuleProto, PacketProto);
-	num_expr_matches(RuleProto, PacketProto)
-alpha_proto(X, X).
+	proto_alpha_num(RuleProto, PacketProto);
+	num_expr_matches(RuleProto, PacketProto).
+proto_alpha_num(X, X).
