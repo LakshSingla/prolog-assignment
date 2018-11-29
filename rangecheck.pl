6de4a2d401db1725ae466f89f63156ab74f34275
Laksh Singla <lakshsingla@gmail.com>
2018-11-29 16:22:53 +0530

    Commented rangecheck.pl

--- rangecheck.pl
+++ rangecheck.pl
@@ -1,11 +1,44 @@
-lies_in_CS_Expr(CSExpr, Val, IsNum) :- split_string(CSExpr, ",", "", L), lies_in_expr(L, Val, IsNum). 
+% Utility to check if a given value lies in a comma seperated expression, 
+% where each expression can be a singleton(representing single entity) 
+% or a dash seperated range. It can work on strings representing numbers (IsNum)
+% or single alphabet (\+IsNum)
 
-lies_in_expr([H|T], Val, IsNum) :- satisfies_maybe_range(H, Val, IsNum); lies_in_expr(T, Val, IsNum).
+lies_in_CS_Expr(CSExpr, Val, IsNum) :- 
 
-satisfies_maybe_range(Expr, Val, IsNum) :- split_string(Expr, "-", "", MaybeRange), satisfies(MaybeRange, Val, IsNum).
+	split_string(CSExpr, ",", "", L),			% split the expression by ','
+	lies_in_expr(L, Val, IsNum).				% check if lies in the list obtained
+
+lies_in_expr([H|T], Val, IsNum) :- 
+
+	satisfies_maybe_range(H, Val, IsNum); 		% true if the value lies in the current expression
+												% OR
+	lies_in_expr(T, Val, IsNum).				% if the value lies in the remaining pool of expressions
+
+satisfies_maybe_range(Expr, Val, IsNum) :- 		% The single expression can be a range or not
+
+	split_string(Expr, "-", "", MaybeRange),	% Split by '-', if array has 1 element then it is a single entity
+												% else it is a range
+	satisfies(MaybeRange, Val, IsNum).
 
 satisfies(MaybeRange, Val, IsNum) :- 
-	IsNum, MaybeRange = [Num1, Num2 | []], number_string(NNum1, Num1), number_string(NNum2, Num2), number_string(NVal, Val), NVal >= NNum1, NVal =< NNum2; 
-	IsNum, MaybeRange = [Num | []], number_string(NNum, Num), number_string(NVal, Val),  NVal=NNum;
-	\+IsNum, MaybeRange = [Alpha1, Alpha2 | []], Val >= Alpha1, Val =< Alpha2; 
-	\+IsNum, MaybeRange = [Alpha | []], Val=Alpha.
+
+	IsNum,										% true if the user wants to compare as numbers
+	MaybeRange = [Num1, Num2 | []], 			% Array has exactly 2 entities
+	number_string(NNum1, Num1), 
+	number_string(NNum2, Num2), 
+	number_string(NVal, Val), 
+	NVal >= NNum1, NVal =< NNum2; 				% Comparison
+
+	IsNum,										% true if the user wants to compare as numbers 
+	MaybeRange = [Num | []], 					% Array has exactly 1 entity
+	number_string(NNum, Num), 
+	number_string(NVal, Val),  
+	NVal=NNum;									% Equality test
+
+	\+IsNum, 									% true if the user wants to compare as single alphabet
+	MaybeRange = [Alpha1, Alpha2 | []], 		% Array has exactly 2 entities
+	Val >= Alpha1, Val =< Alpha2; 				% Comparison
+
+	\+IsNum, 									% true if the user wants to compare as single alphabet
+	MaybeRange = [Alpha | []], 					% Array has exactly 1 entity
+	Val=Alpha.									% Equality
