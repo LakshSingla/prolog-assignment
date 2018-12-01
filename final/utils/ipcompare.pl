21bd2dd6ed195bc606bf6276397b4dda8c12fb3c
Shubham Saxena <shbhmsxn4@gmail.com>
2018-12-01 18:31:38 +0530

    Added hex functionality to ipcompare

--- final/utils/ipcompare.pl
+++ final/utils/ipcompare.pl
@@ -11,6 +11,7 @@
 % :- ensure_loaded([dectobin]).
 :- use_module(dectobin, [decimal_to_byte/2]).
 :- use_module(rangecheck, [num_expr_matches/2]).
+:- use_module(normalize_number, [normalize_numstr/2]).
 
 
 ip_expr_matches(RuleIP, PacketIP) :-
@@ -18,45 +19,40 @@ ip_expr_matches(RuleIP, PacketIP) :-
 	IPList = [_,_|_],
 	ip_list_expr_matches(IPList, PacketIP),
 	!;
-	split_string(RuleIP, "-", "", [Begin, End|[]]),
-	split_string(Begin, ".", "", [B1,B2,B3,B4|[]]),
-	split_string(End, ".", "", [E1,E2,E3,E4|[]]),
-	split_string(PacketIP, ".", "", [P1,P2,P3,P4|[]]),
-	string_numbers_ascending(B1, P1),
-	string_numbers_ascending(B2, P2),
-	string_numbers_ascending(B3, P3),
-	string_numbers_ascending(B4, P4),
-	string_numbers_ascending(P1, E1),
-	string_numbers_ascending(P2, E2),
-	string_numbers_ascending(P3, E3),
-	string_numbers_ascending(P4, E4),
+	split_string(RuleIP, "-", "", [Begin, End]),
+	split_string(Begin, ".", "", BeginList),
+	split_string(End, ".", "", EndList),
+	split_string(PacketIP, ".", "", PacketList),
+	BeginList = [_,_,_,_],
+	EndList = [_,_,_,_],
+	PacketList = [_,_,_,_],
+	ip_list_expr_ascending(BeginList, PacketList),
+	ip_list_expr_ascending(PacketList, EndList),
 	!;
-	split_string(RuleIP, "/", "", [Subnet,MaskStr|[]]),
-	number_string(Mask, MaskStr),
+	split_string(RuleIP, "/", "", [Subnet,MaskStr]),
+	normalize_numstr(MaskStr, Mask),
 	ip_to_binary(Subnet, SubnetBinary),
 	ip_to_binary(PacketIP, PacketIpBinary),
 	masked_compare(SubnetBinary, PacketIpBinary, Mask),
 	!;
-	split_string(RuleIP, ".", "", [R1,R2,R3,R4|[]]),
-	split_string(PacketIP, ".", "", [P1,P2,P3,P4|[]]),
-	string_numbers_equal(R1, P1),
-	string_numbers_equal(R2, P2),
-	string_numbers_equal(R3, P3),
-	string_numbers_equal(R4, P4).
+	split_string(RuleIP, ".", "", [R1,R2,R3,R4]),
+	split_string(PacketIP, ".", "", [P1,P2,P3,P4]),
+	num_expr_matches(R1, P1),
+	num_expr_matches(R2, P2),
+	num_expr_matches(R3, P3),
+	num_expr_matches(R4, P4),
+	!.
 
-% string_numbers_equal(+StringNumber, +StringNumber) : checks if two numbers (stored as strings) are equal.
+% ip_list_expr_ascending(+ListOfStringNumbers, +ListOfStringNumbers) : checks if corresponsing elements of two lists containing strings of numbers are in ascending order.
 
-string_numbers_equal(X, Y) :-
-	number_string(NumX, X),
-	number_string(NumY, Y),
-	NumX = NumY.
-
-% string_numbers_ascending(+StringNumber, +StringNumber) : checks if two numbers (stored as strings) are in ascending order.
-
-string_numbers_ascending(X, Y) :-
-	number_string(NumX, X),
-	number_string(NumY, Y),
-	NumX =< NumY.
+ip_list_expr_ascending(X, Y) :-
+	X = [Xh|Xt],
+	Y = [Yh|Yt],
+	string_concat("0-", Yh, TillYh),
+	num_expr_matches(TillYh, Xh),
+	ip_list_expr_ascending(Xt, Yt);
+	X = [],
+	Y = [].
 
 % ip_list_expr_matches(+IPList, +PacketIP)
 
@@ -77,11 +73,19 @@ masked_compare(Subnet, PacketIP, Mask) :-
 % ip_to_binary(+IPString, -BinaryIPString)
 
 ip_to_binary(IP, IPBinary) :-
-	split_string(IP, ".", "", [ADecimal,BDecimal,CDecimal,DDecimal|[]]),
-	decimal_to_byte(ADecimal, A),
-	decimal_to_byte(BDecimal, B),
-	decimal_to_byte(CDecimal, C),
-	decimal_to_byte(DDecimal, D),
+	split_string(IP, ".", "", [AStr,BStr,CStr,DStr]),
+	normalize_numstr(AStr, ADecNum),
+	normalize_numstr(BStr, BDecNum),
+	normalize_numstr(CStr, CDecNum),
+	normalize_numstr(DStr, DDecNum),
+	number_string(ADecNum, ADecStr),
+	number_string(BDecNum, BDecStr),
+	number_string(CDecNum, CDecStr),
+	number_string(DDecNum, DDecStr),
+	decimal_to_byte(ADecStr, A),
+	decimal_to_byte(BDecStr, B),
+	decimal_to_byte(CDecStr, C),
+	decimal_to_byte(DDecStr, D),
 	string_concat(A, B, AB),
 	string_concat(AB, C, ABC),
 	string_concat(ABC, D, IPBinary).
